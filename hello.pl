c16076d8c998c4f4fa35aa6439146bfb1da347f0
Pyrad <cailong89@163.com>
2016-09-05 22:11:54 +0800

    Add useful scripts of python, perl and tcl

---
+++ hello.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use POSIX
+print "Good day today, Pyrad\n";
+my $date = strftime("%Y-%m-%d",localtime());
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime();
+#print "$date"."\n";
+$mon = $mon + 1;
+$year = $year + 1900;
+print "$year-$mon-$mday $hour:$min:$sec"."\n";
+print "Good bye"."\n";
+
+print "Few days are left"."\n";
