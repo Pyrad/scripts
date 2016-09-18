d438ae0af380050829eaed798a82f0140b45d38a
Pyrad <cailong89@163.com>
2016-09-18 22:04:35 +0800

    update git commands

--- git_commands.sh
+++ git_commands.sh
@@ -9,6 +9,9 @@ git add $FILENAME
 git commit -m "Add file for first time"
 git push -u origin master
 
+### connect to a github repo
+git remote add origin git@github.com:Pyrad/others.git
+
 ### Clone a github repo
 git clone git@github.com:Pyrad/scripts.git
 
