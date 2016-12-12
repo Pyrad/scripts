679765e98951b73896ad66feb0ebea20611ac896
Pyrad <cailong89@163.com>
2016-12-13 00:31:41 +0800

    general github steps clue

--- git_commands.sh
+++ git_commands.sh
@@ -17,3 +17,12 @@ git clone git@github.com:Pyrad/scripts.git
 
 ### Download changes from remote
 git pull origin master
+
+
+### General steps
+echo "# c_cpp_notes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:Pyrad/<REPO_NAME>.git
+git push -u origin master ###Before this you should create repo <REPO_NAME> first in github manually via website
