bef19bfee66f0217b5fe5d55e1efc08b44d4cd0d
pyrad <cailong89@163.com>
2020-06-30 16:54:25 +0800

    How to create a new branch & push to GitHub

--- git_commands.sh
+++ git_commands.sh
@@ -45,3 +45,27 @@ git clean -f <path>
 git clean -df
 ### Tells which files will be removed, just give the information but not remove indeed
 git clean -n
+
+### Create a new branch and switch to that branch
+git checkout -b <NEW_BRANCH_NAME>
+### Create a new branch and DON'T switch to that branch
+git branch <NEW_BRANCH_NAME>
+### Switch to another branch
+git checkout <OTHER_BRANCH_NAME>
+
+### Create a new branch in local and then push it to the remote (GitHub) and then track it
+# 1. Create a new branch in local
+git branch <NEW_BRANCH_NAME>
+# 2. Get the remote repo name
+git remote
+# 3. Push the new branch to remote repo
+git push <REMOTE_REPO_NAME> <NEW_BRANCH_NAME>
+# Usually remote repo name is 'origin'
+git push origin <NEW_BRANCH_NAME>
+# 4. Connect this new local branch with the new branch just pushed to the remote repo
+git branch --set-upstream-to=origin/<NEW_BRANCH_NAME>
+# 5. After that, modify code and push as usaul
+git add -A
+git commit -m "XXXXX"
+git push (or use git push origin <NEW_BRANCH_NAME>)
+
