78b75687964b3b5759d7e416646870927258b59e
pyrad.asus.msys2 <cailong89@163.com>
2022-06-26 21:31:28 +0800

    Move content to bash_colors.sh
    Move the content from file shell.colors.sh to bash_colors.sh, as
    a independent function, and call it in this file.
    
    Jus to reduce file numbers

--- bash_colors.sh
+++ bash_colors.sh
@@ -1,5 +1,31 @@
-### Show different kinds of colors
+#!/bin/sh -
 
+
+######################################################################
+# Function print_bash_colors_0 comes from others, keep it here as a 
+# reminder. This original was located in another file, move it here
+# to reduce file numbers. 2022-06-26  
+######################################################################
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+function print_bash_colors_0() {
+	for fgbg in 38 48 ; do # Foreground / Background
+		for color in {0..255} ; do # Colors
+			# Display the color
+			printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+			# Display 6 colors per lines
+			if [ $((($color + 1) % 6)) == 4 ] ; then
+				echo # New line
+			fi
+		done
+		echo # New line
+	done
+}
+
+### Show different kinds of colors
 function print_bash_colors() {
 	cReset='\033[0m'
 	cPrefix='\033['
@@ -57,4 +83,13 @@ function print_bash_colors() {
 
 
 ### Call the function to display colors in bash
+echo "--------------------------------------------------"
+echo " Show colors by function print_bash_colors"
+echo "--------------------------------------------------"
 print_bash_colors
+
+echo ""
+echo "--------------------------------------------------"
+echo " Show colors by function print_bash_colors_0"
+echo "--------------------------------------------------"
+print_bash_colors_0
