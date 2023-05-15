9d2cff57880e2c1863585bd525097a7dc31d269b
pyrad.xiaoxin.msys2 <cailong89@163.com>
2023-05-15 11:09:34 +0800

    Add a function to show CPU name

--- script_python/win32_get_cpu_info.py
+++ script_python/win32_get_cpu_info.py
@@ -2,13 +2,30 @@
 This needs Python 3 and module pywin32
 """
 
+import os, platform, subprocess, re
+
 def get_cpu_type():
     from win32com.client import GetObject
     root_winmgmts = GetObject("winmgmts:root\cimv2")
     cpus = root_winmgmts.ExecQuery("Select * from Win32_Processor")
     return cpus[0].Name
 
+def get_cpu_name():
+    if platform.system() == "Windows":
+        return platform.processor()
+    elif platform.system() == "Darwin":
+        os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+        command = "sysctl -n machdep.cpu.brand_string"
+        return subprocess.check_output(command).strip()
+    elif platform.system() == "Linux":
+        command = "cat /proc/cpuinfo"
+        all_info = subprocess.check_output(command, shell=True).decode().strip()
+        for line in all_info.split("\n"):
+            if "model name" in line:
+                return re.sub(".*model name.*:", "", line, 1)
+    return ""
 
 if __name__ == "__main__":
     print(f"Current machine's CPU is {get_cpu_type()}")
+    print(f"Current machine's CPU name {get_cpu_name()}")
 
