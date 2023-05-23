d3f8647513e7d425371cb22a9fafc701914e05ea
pyrad.xiaoxin.msys2 <cailong89@163.com>
2023-05-23 13:25:00 +0800

    Update bash function related

--- bash_setup/func_args.sh
+++ bash_setup/func_args.sh
@@ -10,9 +10,19 @@ function foo2() {
     return 1
 }
 
+function foo3() {
+	echo "Number of args: $#"
+	for arg in $@; do
+		echo "Arg: $arg"
+	done
+}
+
 
 foo "a" "b" "c"
 echo "Function return value is: $?"
 
 foo2 "a" "b" "c"
+echo "Function return value is: $?"
+
+foo3 "a" "b" "c"
 echo "Function return value is: $?"
